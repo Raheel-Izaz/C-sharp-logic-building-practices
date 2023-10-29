b22dac4dc64ba603c0fb382865b9d8c3bc0600c3
Raheel-Izaz <raheelizaz3@gmail.com>
2023-10-29 12:45:41 +0500

    C# Logic building practices

---
+++ ProblemNo.1/ProblemNo.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemNo._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Problem No.1
+
+            int a = 0;
+            for (int i = 1; i < 1000; i++) 
+            {
+
+
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    a += i;
+
+                    //a = a + i;
+                }
+            }
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ ProblemNo.3/ProblemNo.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemNo._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           // Problem No.3
+
+           long num = 600851475143;
+           long largestPF = 0;
+           long divisor = 2;
+
+
+           while (num > 1)
+           {
+               if (num % divisor == 0)
+               {
+                   num = num / divisor;
+                   largestPF = divisor;
+               }
+               else
+               {
+                   divisor++;
+               }
+           }
+           Console.WriteLine($"Largest Prime Factor Of 600851475143 is {largestPF}" ) ;
+
+           
+        }
+    }
+}
